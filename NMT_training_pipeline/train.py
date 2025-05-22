380854c8e1530df69ba7b555e5a534586505ae2e
sandrapoer <sandrapoernbacher@gmail.com>
2025-05-22 11:14:14 +0000

    altered pipelines

--- NMT_training_pipeline/train.py
+++ NMT_training_pipeline/train.py
@@ -3,10 +3,8 @@ import os
 from datasets import load_dataset
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainer, Seq2SeqTrainingArguments, DataCollatorForSeq2Seq
 from transformers import set_seed
-import optuna
 from sacrebleu import corpus_bleu
-import datetime
-from tqdm import tqdm
+import time
 from functools import partial
 
 def load_config(config_path):
@@ -103,17 +101,17 @@ def compute_metrics(eval_preds, tokenizer):
         #"batch_size": batch_size,
             #"num_train_epochs": num_train_epochs
     # })
-def train(model, tokenizer, tokenized_train, tokenized_val, data_collator, learning_rate, weight_decay, batch_size, num_epoch, seed = 224):
+def train(model, tokenizer, output_dir, logging_dir, tokenized_train, tokenized_val, data_collator, learning_rate, weight_decay, batch_size, num_epochs, seed = 224):
     training_args = Seq2SeqTrainingArguments(
-        output_dir=f"NMT_training_pipeline/trained_model",
-        logging_dir=f"NMT_training_pipeline/logs",
+        output_dir=output_dir,
+        logging_dir=logging_dir,
         eval_strategy="epoch",
         save_strategy="no",
         learning_rate=learning_rate,
         weight_decay=weight_decay,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
-        num_train_epochs=num_epoch,
+        num_train_epochs=num_epochs,
         logging_strategy="steps",
         logging_steps=40,
         report_to="tensorboard",
@@ -132,21 +130,22 @@ def train(model, tokenizer, tokenized_train, tokenized_val, data_collator, learn
     )
 
         
-    trainer.train()
+    train_result = trainer.train()
     # Save the model
     trainer.save_model()
     # Evaluate and return BLEU score
     eval_results = trainer.evaluate()
     bleu_score = eval_results.get("eval_bleu", None)
+    train_loss = train_result.training_loss if hasattr(train_result, "training_loss") else None
     print(f"BLEU score: {bleu_score}")
-    return bleu_score
+    return bleu_score, train_loss
 
   
 def main():
     # Initialize WandB
     #wandb.init(project="ML for Translation Task", name="Model Training")
     summary_path = "NMT_training_pipeline/training_summary.tsv"
-    config_path = "NMT_training_pipeline/configs/nllb_config.yml"
+    config_path = "NMT_training_pipeline/configs/mbart50_config.yml"
     print("Loading configurations:")
 
 
@@ -155,16 +154,18 @@ def main():
 
     # Load tokenizer and model
     tokenizer = AutoTokenizer.from_pretrained(config["tokenizer_name"])
-    model = AutoModelForSeq2SeqLM.from_pretrained(config["model_name_or_path"])
+    model = AutoModelForSeq2SeqLM.from_pretrained(config["model_name"])
     model_type = config["model_type"]
-    src_lang_code = config.get["src_lang_code", ""]
-    tgt_lang_code = config.get["src_lang_code", ""]
+    src_lang_code = config.get("src_lang_code", "")
+    tgt_lang_code = config.get("tgt_lang_code", "")
     src_lang = config['source_lang']
     tgt_lang = config['target_lang']
     max_src_length = config["max_source_length"]
     max_tgt_length = config["max_target_length"]
     train_file = config["train_file"]
     val_file = config["val_file"]
+    output_dir = config["output_dir"]
+    logging_dir = config["logging_dir"]
 
     if src_lang_code and tgt_lang_code:
         tokenizer.src_lang = src_lang_code
@@ -176,14 +177,14 @@ def main():
     # Load and preprocess data
     train_dataset, val_dataset = get_train_val_datasets(train_file, val_file)
     tokenized_train = preprocess_and_tokenize(train_dataset, model_type, tokenizer, src_lang, tgt_lang, max_src_length, max_tgt_length, desc="Tokenizing train dataset")
-    tokenized_val = preprocess_and_tokenize(val_dataset, model_type, tokenizer, src_lang_code, tgt_lang_code, max_src_length, max_tgt_length, desc="Tokenizing validation dataset")
+    tokenized_val = preprocess_and_tokenize(val_dataset, model_type, tokenizer, src_lang, tgt_lang, max_src_length, max_tgt_length, desc="Tokenizing validation dataset")
 
     # Data collator
     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
-    learning_rate = config["learning_rate"]
-    weight_decay = config["weight_decay"]
-    batch_size = config["batch_size"]
-    num_epoch = config["num_epoch"]
+    learning_rate = float(config["learning_rate"])
+    weight_decay = float(config["weight_decay"])
+    batch_size = int(config["batch_size"])
+    num_epochs = int(config["num_train_epochs"])
 
     #study = optuna.create_study(direction="maximize")  # Maximize BLEU score
     #study.optimize((partial(
@@ -202,33 +203,49 @@ def main():
     #for key, value in study.best_trial.params.items():
        # print(f"    {key}: {value}")
         #config[key] = value  
-    bleu_score = train(
+    start_time = time.time()
+    bleu_score, train_loss = train(
         model, 
         tokenizer, 
+        output_dir, 
+        logging_dir, 
         tokenized_train, 
         tokenized_val, 
         data_collator, 
         learning_rate, 
         weight_decay, 
         batch_size, 
-        num_epoch  
+        num_epochs, 
+        seed = 224 
     )
     
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    training_time = time.time() - start_time
+    minutes, seconds = divmod(int(training_time), 60)
+    training_time_str = f"{minutes:02d}:{seconds:02d}"
+    
+    
+    row = "{:<40}{:<16}{:<16}{:<13}{:<15}{:<12}{:<12}{:<12}\n".format(
+        config['model_name'],
+        training_time_str,
+        f"{train_loss:.2f}" if train_loss is not None else "NA",
+        f"{bleu_score:.2f}" if bleu_score is not None else "NA",
+        learning_rate,
+        batch_size,
+        num_epochs,
+        weight_decay
+    )
+    
+    
     write_header = not os.path.exists(summary_path)
     with open(summary_path, "a", encoding="utf-8") as f:
         if write_header:
-            f.write("model\ttime\tbleu_score\tlearning_rate\tbatch_size\tnum_epoch\tweight_decay\n")
-        f.write(f"{model}\t{timestamp}\t{bleu_score}\t{learning_rate}\t{batch_size}\t{num_epoch}\t{weight_decay}\n")
+            header = "{:<40}{:<16}{:<16}{:<13}{:<15}{:<12}{:<12}{:<12}\n".format(
+                "model", "train_time", "train_loss","bleu_score", "learning_rate", "batch_size", "num_epochs", "weight_decay"
+                )
+            f.write(header)
+        f.write(row)
     
     
-    # Add date/time and BLEU score to config
-    #config["last_trained"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    #config["bleu_score"] = bleu_score
-
-    #with open(config_path, "w") as file:
-        #yaml.dump(config, file)
-
-    #print(f"Updated config saved to {config_path}")
+  
 if __name__ == "__main__":
     main()
